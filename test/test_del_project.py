85a00f53d00227847078bfdeb1724a4c928022b3
Elissara <68391346+Elissara@users.noreply.github.com>
2021-04-13 21:07:38 +0300

    Реализована загрузка списка проектов через SOAP

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -2,14 +2,14 @@ from fixture.project import Project
 import random
 
 
-def test_del_project(app, db):
+def test_del_project(app):
     app.session.login("administrator", "root")
-    if len(db.get_projects_list()) == 0:
+    if len(app.soap.get_projects_list()) == 0:
         app.project.create(Project(name="Test_project_007"))
-    old_list_of_projects = db.get_projects_list()
+    old_list_of_projects = app.soap.get_projects_list()
     project = random.choice(old_list_of_projects)
     app.project.delete(project.id)
-    new_list_of_projects = db.get_projects_list()
+    new_list_of_projects = app.soap.get_projects_list()
     old_list_of_projects.remove(project)
     assert sorted(old_list_of_projects, key=Project.max_id) == sorted(new_list_of_projects, key=Project.max_id)
 
