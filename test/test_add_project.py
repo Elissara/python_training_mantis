201a98131fbd791a122968580589afbadf4e7dc1
Elissara <68391346+Elissara@users.noreply.github.com>
2021-04-13 20:47:08 +0300

    Реализованы тесты добавления и удаления проектов

---
+++ test/test_add_project.py
@@ -0,0 +1,11 @@
+from model.project import Project
+
+
+def test_add_project(app, db, json_projects):
+    app.session.login("administrator", "root")
+    old_list_of_projects = db.get_projects_list()
+    project_data = json_projects
+    app.project.create(project_data)
+    new_list_of_projects = db.get_projects_list()
+    old_list_of_projects.append(project_data)
+    assert sorted(old_list_of_projects, key=Project.max_id) == sorted(new_list_of_projects, key=Project.max_id)
---
+++ test/test_del_project.py
@@ -0,0 +1,15 @@
+from fixture.project import Project
+import random
+
+
+def test_del_project(app, db):
+    app.session.login("administrator", "root")
+    if len(db.get_projects_list()) == 0:
+        app.project.create(Project(name="Test_project_007"))
+    old_list_of_projects = db.get_projects_list()
+    project = random.choice(old_list_of_projects)
+    app.project.delete(project.id)
+    new_list_of_projects = db.get_projects_list()
+    old_list_of_projects.remove(project)
+    assert sorted(old_list_of_projects, key=Project.max_id) == sorted(new_list_of_projects, key=Project.max_id)
+
