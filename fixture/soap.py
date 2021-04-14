aae9468588f6c8bc502ea8681c1f1b36d708f32a
Elissara <68391346+Elissara@users.noreply.github.com>
2021-04-14 22:20:47 +0300

    Доработана фикстура soap.py

--- fixture/soap.py
+++ fixture/soap.py
@@ -12,7 +12,7 @@ class SoapHelper:
 
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config['web']['baseUrl'] + self.app.config['web']['wsdl'])
         try:
             client.service.mc_login(username, password)
             return True
@@ -22,7 +22,7 @@ class SoapHelper:
 
     def get_projects_list(self):
         list = []
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config['web']['baseUrl'] + self.app.config['web']['wsdl'])
         try:
             data = client.service.mc_projects_get_user_accessible(self.app.config['webadmin']['username'],
                                                                       self.app.config['webadmin']['password'])
